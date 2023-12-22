fe69a5620a21d8aac74d47a54e65af9a42432b5f
haseeb hummo <haseebpersonalwork@gmail.com>
2023-12-22 10:53:46 +0530

    Move product route handlers to dedicated controller file.

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+import Product from "../models/productModel.js";
+import wrapAsync from "../utils/wrapAsync.js";
+
+// Fetch all products
+// Route: GET /api/products
+const getProducts = wrapAsync(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+});
+
+// Fetch a product by ID
+// Route: GET /api/products/:id
+const getProductById = wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (product) {
+        return res.json(product);
+    }
+
+    throw new Error("Resource not found");
+});
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,9 @@
 import express from 'express';
-import Product from '../models/productModel.js';
-import wrapAsync from '../utils/wrapAsync.js';
-
+import {getProducts , getProductById} from '../controllers/productController.js'
 const router = express.Router();
 
-router.get('/', wrapAsync(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-}));
+router.route('/').get(getProducts);
 
-router.get('/:id', wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-
-    if (product) {
-        return res.json(product);
-    }
-
-    throw new Error("Resource not found");
-}));
+router.route('/:id').get(getProductById);
 
 export default router;
