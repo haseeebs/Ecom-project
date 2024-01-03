eb3b680e925be55043455cb08700be255c0a6c50
haseeb hummo <haseebpersonalwork@gmail.com>
2024-01-03 13:17:54 +0530

    Implement getOrderById controller to retrieve an order by ID and Implement createOrder controller to handle the creation of a new order.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,41 +4,75 @@ import wrapAsync from '../utils/wrapAsync.js';
 // Create new order
 // Route: POST /api/orders
 // Access Private
-export const createOrder = wrapAsync( async(req , res) => {
-    res.send("Create order");
+export const createOrder = wrapAsync(async (req, res) => {
+    const {
+        orderItems,
+        itemsPrice,
+        paymentMethod,
+        shippingAddress,
+        shippingPrice,
+        taxPrice,
+        totalPrice
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Error("No order items")
+    } else {
+        const newOrder = new Order({
+            user: req.user._id,
+            orderItems: orderItems.map(orderItem => ({ ...orderItem, product: orderItem._id, _id: undefined })),
+            itemsPrice,
+            paymentMethod,
+            shippingAddress,
+            shippingPrice,
+            taxPrice,
+            totalPrice
+        })
+
+        const createdOrder = await newOrder.save();
+        res.status(201).json({ order: createdOrder, message: 'Order created successfully' });
+    }
 })
 
 // Get logged in user orders
 // Route: GET /api/orders/myorders
 // Access Private
-export const getMyOrders = wrapAsync( async(req , res) => {
-    res.send("Get my orders");
+export const getMyOrders = wrapAsync(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders)
 })
 
 // Get order by id
 // Route: GET /api/orders/:id
 // Access Private
-export const getOrderById = wrapAsync( async(req , res) => {
-    res.send("Get order by id");
+export const getOrderById = wrapAsync(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+    if (order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404)
+        throw new Error('Order nor found')
+    }
 })
 
 // Update order to paid
 // Route: GET /api/orders/:id/pay
 // Access Private
-export const updateOrderToPaid = wrapAsync( async(req , res) => {
+export const updateOrderToPaid = wrapAsync(async (req, res) => {
     res.send("Update order to paid");
 })
 
 // Update order to delivered
 // Route: GET /api/orders/:id/deliver
 // Access Private/Admin
-export const updateOrderToDelivered = wrapAsync( async(req , res) => {
+export const updateOrderToDelivered = wrapAsync(async (req, res) => {
     res.send("Update order to delivered");
 })
 
 // Get all orders
 // Route: GET /api/orders
 // Access Private/Admin
-export const getAllOrders = wrapAsync( async(req , res) => {
+export const getAllOrders = wrapAsync(async (req, res) => {
     res.send("Get all orders");
 })
--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -11,66 +11,66 @@ const orderSchema = new mongoose.Schema({
             name: { type: String, required: true },
             quantity: { type: Number, required: true },
             image: { type: String, required: true },
-            price: { type : Number, required: true },
+            price: { type: Number, required: true },
             product: {
                 type: mongoose.Schema.Types.ObjectId,
                 ref: "Product"
             },
         }
     ],
-    shippingAddress : {
-        country : {type : String , required : true},
-        city : {type : String , required : true},
-        postalCode : {type : String , required : true},
-        address : {type : String, required : true},
+    shippingAddress: {
+        country: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        address: { type: String, required: true },
     },
-    paymentMethod : {
-        type : String,
-        required : true
+    paymentMethod: {
+        type: String,
+        required: true
     },
-    paymentResult : {
-        id : {type : String},
-        status : {type : String},
-        update_time : {type : String},
-        email : {type : String}
+    paymentResult: {
+        id: { type: String },
+        status: { type: String },
+        update_time: { type: String },
+        email: { type: String }
     },
-    itemPrice : {
-        type : Number,
-        required : true,
-        default : 0.0
+    itemPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
     },
-    taxPrice : {
-        type : Number,
-        required : true,
-        default : 0.0
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
     },
-    shippingPrice : {
-        type : Number,
-        required : true,
-        default : 0.0
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
     },
-    totalPrice : {
-        type : Number,
-        required : true
+    totalPrice: {
+        type: Number,
+        required: true
     },
-    isPaid : {
-        type : Boolean,
-        required : true,
-        default : false
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false
     },
-    paidAt : {
-        type : Date
+    paidAt: {
+        type: Date
     },
-    isDelivered : {
-        type : Boolean,
-        required : true,
-        default : false
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false
     },
-    deliveredAt : {
-        type : Date
+    deliveredAt: {
+        type: Date
     }
-}, {timestamps : true});
+}, { timestamps: true });
 
-const Order = mongoose.model("Order" , orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 
 export default Order;
