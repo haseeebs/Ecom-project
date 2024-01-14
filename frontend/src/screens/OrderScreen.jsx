e94fd3e05d37a60fd3265edfb4ba12760b4070b6
haseeb hummo <haseebpersonalwork@gmail.com>
2024-01-15 04:48:56 +0530

    Implement PayPal payment handling in OrderScreen.

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -44,6 +44,40 @@ const OrderScreen = () => {
 
     }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
 
+    const onApproveTest = async () => {
+        await payOrder({ orderId, details: { payer: {} } });
+        refetch();
+        toast.success('Payment Successful');
+    }
+
+    const onApprove = (data, actions) => {
+        return actions.order.capture().then(async function (details) {
+            try {
+                await payOrder({ orderId, details });
+                refetch();
+                toast.success('Payment Successful');
+            } catch (error) {
+                toast.error(error?.data?.message || error.message);
+            }
+        })
+    }
+
+    const createOrder = (data, actions) => {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: order.totalPrice
+                    }
+                }
+            ]
+        }).then((orderId) => orderId)
+    }
+
+    const onError = (error) => {
+        toast.error(error.message);
+    }
+
     return isLoading ? (
         <Loader />
     ) : error ? (
@@ -86,7 +120,7 @@ const OrderScreen = () => {
                                 {order.paymentMethod}
                             </p>
                             {order.isPaid ? (
-                                <Message variant="success">Paid on {order.PaidAt}</Message>
+                                <Message variant="success">Paid on {order.paidAt}</Message>
                             ) : (
                                 <Message variant="danger">Not Paid</Message>
                             )}
@@ -143,13 +177,32 @@ const OrderScreen = () => {
                                     <Col>${order.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
+
+                            {!order.isPaid && (
+                                <ListGroup.Item>
+                                    {loadingPay && <Loader />}
+
+                                    {isPending ? <Loader /> : (
+                                        <div>
+                                            <Button onClick={onApproveTest} style={{ marginBottom: '10px' }}>Test Pay Order</Button>
+
+                                            <div>
+                                                <PayPalButtons createOrder={createOrder} onApprove={onApprove} onError={onError}></PayPalButtons>
+                                            </div>
+                                        </div>
+
+
+                                    )}
+                                </ListGroup.Item>
+                            )}
+
                         </ListGroup>
-                        {/* PAY ORDER PLACEHOLDER */}
+
                         {/* MARK AS DELIVERED PLACEHOLDER */}
 
                     </Card>
                 </Col>
-            </Row>
+            </Row >
         </>
     );
 };
